6ee493c81bb1c548221b6649b4ead69f90f04f42
Pablo <33316949+martacoe@users.noreply.github.com>
2018-03-01 19:13:09 +0100

    a ver si ahora va, pesao

---
+++ src/persistencia/SQLManager.java
@@ -0,0 +1,70 @@
+package persistencia;
+
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+
+import dominio.*;
+
+public class SQLManager {
+
+	static Connection conexion = null;
+	static Statement stt = null;
+
+	public static void conectar() {
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1/fifa18", "root", "");
+			stt = conexion.createStatement();
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error de BD", JOptionPane.ERROR_MESSAGE);
+
+		}
+	}
+
+	public static void desconectar() {
+		try {
+
+			if (conexion != null && stt != null) {
+				stt.close();
+				conexion.close();
+			}
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error de BD", JOptionPane.ERROR_MESSAGE);
+
+		}
+
+	}
+
+	public static ResultSet select(String tabla, String condicion) {
+		ResultSet rs = null;
+		
+		try {
+			conectar();
+			rs = stt.executeQuery("select * from " + tabla + " where " + condicion);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error de BD", JOptionPane.ERROR_MESSAGE);
+
+		}
+		return rs;
+	}
+
+	public static ResultSet select(String tabla) {
+		ResultSet rs = null;
+		try {
+			conectar();
+			rs = stt.executeQuery("select * from " + tabla+" order by Overall desc");
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error de BD", JOptionPane.ERROR_MESSAGE);
+
+		}
+		return rs;
+	}
+
+}
